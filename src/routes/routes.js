e4acffb08d9637794de4e8afa4a8e588e660b7a9
Livio Ribeiro <liviomreboucas@gmail.com>
2024-06-12 16:37:22 -0300

    tetativa 10 alteracao pagina de routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,7 +10,7 @@ const AppRoutes = () => {
     <Router>
       <Fragment>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<PrivateRoute />} />
           <Route path="/produtos" element={<Products />} />
           <Route path="/carrinho" element={<Cart />} />
           <Route element={<PrivateRoute />}>

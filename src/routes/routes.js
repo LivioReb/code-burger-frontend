0bddb84017b2a3fa62401713c116d9f78fb0eabd
Livio Ribeiro <liviomreboucas@gmail.com>
2024-06-12 15:44:33 -0300

    tentativa 4 alterando rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,11 +9,11 @@ const AppRoutes = () => {
   return (
     <Router>
       <Fragment>
-        <Routes>
-          <Route path="/" element={<Home />}>
-            <Route index element={<Home />} />
+        <Routes> 
+          <Route index element={<Home />} />
             <Route path="/produtos" element={<Products />} />
             <Route path="/carrinho" element={<Cart />} />
+          <Route path="/" element={<PrivateRoute />}>
             <Route path={paths.Order} element={<Admin />} />
             <Route path={paths.Products} element={<Admin />} />
             <Route path={paths.NewProduct} element={<Admin />} />
